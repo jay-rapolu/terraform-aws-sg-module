9f6c79119962924ee67e5d15d8b4fb8c660b0f84
jay-rapolu <jay.rapolu@outlook.com>
2025-07-05 09:04:47 +0530

    terraform sg

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+
+  tags = merge(
+    local.common_variables,
+    {
+    Name = "${var.project}-${var.environment}-${var.sg_name}"
+  })
+}
